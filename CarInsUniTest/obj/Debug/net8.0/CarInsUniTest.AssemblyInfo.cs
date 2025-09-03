01f79effb5ab16861b892187625995c9cd059953
burlacuvlad1998-cmyk <burlacu.vlad1998@gmail.com>
2025-09-03 22:02:06 +0300

    Add gitignore

--- CarInsUniTest/obj/Debug/net8.0/CarInsUniTest.AssemblyInfo.cs
+++ CarInsUniTest/obj/Debug/net8.0/CarInsUniTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarInsUniTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d38afebb51f52549a170ae009aaafa402e92a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarInsUniTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarInsUniTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CarInsurance.Api/obj/Debug/net8.0/CarInsurance.Api.AssemblyInfo.cs
+++ CarInsurance.Api/obj/Debug/net8.0/CarInsurance.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarInsurance.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d38afebb51f52549a170ae009aaafa402e92a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarInsurance.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarInsurance.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
